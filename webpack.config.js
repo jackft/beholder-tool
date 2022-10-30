a574802dbcc56d70b864ba28416401eca32e487d
Jack Terwilliger <jack.f.terwilliger@gmail.com>
2022-10-30 14:14:46 -0700

    dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   mode: "development",
   output: {
-    path: path.resolve(__dirname, 'bundles'),
+    path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     libraryTarget: 'umd',
     library: 'psychCoder',
